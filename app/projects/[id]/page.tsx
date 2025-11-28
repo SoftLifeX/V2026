4abdaa499c8bb9fa6e4c0f6695631039e576edd1
Daniel Daniel c <softlife.dev001@gmail.com>
2025-11-28 11:54:04 +0100

    Add files via upload

---
+++ app/projects/[id]/page.tsx
@@ -0,0 +1,7 @@
+import ProjectDetailPage from "@/features/projects/[id]/page";
+import { generateStaticParams, generateMetadata } from "@/features/projects/[id]/page";
+
+// Re-export metadata and static params generation
+export { generateStaticParams, generateMetadata };
+
+export default ProjectDetailPage;
